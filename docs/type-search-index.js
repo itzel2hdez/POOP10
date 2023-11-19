8c09ee3ef6ccb4de3cdac68b7f260fc4c67050df
itzel2hdez <112337073+itzel2hdez@users.noreply.github.com>
2023-11-19 17:23:15 -0600

    Adición de javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop10","l":"Cajero"},{"p":"poop10","l":"Cuenta"},{"p":"poop10","l":"POOP10"},{"p":"poop10","l":"SaldoInsuficienteException"}];updateSearchResults();
